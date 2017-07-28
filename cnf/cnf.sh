00edfd056347ddc00e0a6f88324b10922c8e6d95
Jeremy Hajek <hajek@iit.edu>
2017-07-28 10:49:48 -0500

    Added URLs in the comments

--- cnf/cnf.sh
+++ cnf/cnf.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Inject the username and password for autologin later in a ~/.my.cnf file
+# http://serverfault.com/questions/103412/how-to-change-my-mysql-root-password-back-to-empty/103423#103423
+# https://stackoverflow.com/questions/8020297/mysql-my-cnf-file-found-option-without-preceding-group
+
 echo -e "[mysqld]\n\n" > ~/.my.cnf
 echo -e "[client]\nuser = root\npassword = $DBPASS" >> ~/.my.cnf
 echo -e "\nport = 3306\nsocket = /var/run/mysqld/mysqld.sock\n" >> ~/.my.cnf
