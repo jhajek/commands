50b7c2ae4a5e4c137b403fe712188198dfb240a2
Jeremy Hajek <hajek@iit.edu>
2017-08-01 01:54:54 -0500

    Added correct character and collation set

--- cnf/cnf.sh
+++ cnf/cnf.sh
@@ -5,9 +5,20 @@
 # https://stackoverflow.com/questions/8020297/mysql-my-cnf-file-found-option-without-preceding-group
 
 echo -e "[mysqld]\n\n" > ~/.my.cnf
-echo -e "[client]\nuser = root\npassword = $DBPASS" >> ~/.my.cnf
+echo -e "\ninnodb_file_format = Barracuda" >> ~/.my.cnf
+echo -e "\ninnodb_file_per_table = 1" >> ~/.my.cnf
+echo -e "\ninnodb_large_prefix" >> ~/.my.cnf
+echo -e "\ncharacter-set-server = utf8mb4" >> ~/.my.cnf
+echo -e "\ncollation-server = utf8mb4_unicode_ci" >> ~/.my.cnf
+echo -e "\nskip-character-set-client-handshake" >> ~/.my.cnf
+echo -e "\n[client]\nuser = root\npassword = $DBPASS" >> ~/.my.cnf
 echo -e "\nport = 3306\nsocket = /var/run/mysqld/mysqld.sock\n" >> ~/.my.cnf
+echo -e "\ndefault-character-set = utf8mb4\n" >> ~/.my.cnf
 
 echo -e "[mysqld]\n\n" > ~/.my.cnf.backup
 echo -e "[client]\nuser = backup\npassword = $BKPASS" >> ~/.my.cnf.backup
 echo -e "\nport = 3306\nsocket = /var/run/mysqld/mysqld.sock\n" >> ~/.my.cnf.backup
+echo -e "\ndefault-character-set = utf8mb4\n" >> ~/.my.cnf.backup
+
+
+
